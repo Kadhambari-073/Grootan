7a987a4417fd215948f128c550e9b7154d83e56d
Kadhambari-073 <72736221+Kadhambari-073@users.noreply.github.com>
2021-06-15 10:48:32 +0530

    Add files via upload

---
+++ src/pages/about.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const About = () => {
+return (
+	<div
+	style={{
+		display: 'flex',
+		justifyContent: 'Right',
+		alignItems: 'Right',
+		height: '100vh'
+	}}
+	>
+	<pre>            <h1><i>Welcome you all!!</i></h1>
+
+  <p><b>Click user details to view list of users</b></p>
+  </pre>
+  </div>
+);
+};
+
+export default About;
